7bdd73946c2b4342cf32af5dfbc1eb64001690a9
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-03 17:12:50 +0530

    first commit

---
+++ ubuntu_initial_setup.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+echo "Updating the repositories..."
+sudo apt update
+
+echo "Upgrading..."
+sudo apt upgrade -y
+
+#echo "Installing docker..."
+#sudo apt install docker.io -y
+
+#echo "Installing docker-compose..."
+#sudo apt install docker-compose -y
+
+echo "Installing firewalld utility..."
+sudo apt install firewalld -y
