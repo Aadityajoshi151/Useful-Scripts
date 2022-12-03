177b73c61fe7ba4039a6c31ac96ade85e2d1bf6c
Aadityajoshi151 <aadityajoshi151@gmail.com>
2022-12-03 17:26:01 +0530

    uncomment docker installation

--- ubuntu_initial_setup.sh
+++ ubuntu_initial_setup.sh
@@ -6,11 +6,11 @@ sudo apt update
 echo "Upgrading..."
 sudo apt upgrade -y
 
-#echo "Installing docker..."
-#sudo apt install docker.io -y
+echo "Installing docker..."
+sudo apt install docker.io -y
 
-#echo "Installing docker-compose..."
-#sudo apt install docker-compose -y
+echo "Installing docker-compose..."
+sudo apt install docker-compose -y
 
 echo "Installing firewalld utility..."
 sudo apt install firewalld -y
